9f077bd948f9fa03e456f2c20adc774f656a913b
Diksha6960 <118951869+Diksha6960@users.noreply.github.com>
2022-12-22 23:53:59 +0530

    assignment done

---
+++ while_DoWhile_loop_ass.js
@@ -0,0 +1,50 @@
+console.log(`###1. Print number from 5 to 15 by incrementing 1`)
+var i=5;
+while (i<=15) {
+    console.log(i)
+    i++
+    
+}
+console.log(`###2. Print number from 50to 40 by decrementing 1`)
+var i=50;
+while (i>=40) {
+    console.log(i)
+    i--
+    
+}
+console.log(`###3. Find first 15 odd no `)
+var i=1;
+while (i<=29) {
+    console.log(i)
+    i=i+2
+    
+}
+console.log(`###4. Find first 10 even no `)
+index=2
+do {
+    console.log(index);
+    index=index+2
+    
+} while (index<=20);
+console.log(`###5.Print the table of 5`)
+index=5;
+do {
+    console.log(index)
+    index=index+5
+
+    
+} while (index<=50);
+console.log(`###5.Print the table of 10`)
+index=10;
+do {
+    console.log(index)
+    index=index+10
+} while (index<=100);
+
+console.log(`###7.Print the table of 10 in reveres order`)
+
+index = 10;
+do {console.log(index)
+    index=index-1
+    
+} while (index>=1);
